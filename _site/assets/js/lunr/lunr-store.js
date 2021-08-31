47d1ae31048f01635a7b017ba7421142e351fd10
y00njae <rladbswo12@gist.ac.kr>
2021-08-31 15:59:54 +0900

    1. code font changed, 2. post update which explains how to change code font

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,27 +5,33 @@ var store = [{
         "url": "/blog/blog-plan/",
         "teaser": "/assets/images/default.jpeg"
       },{
-        "title": "리액트 입문",
+        "title": "[React] 리액트 입문",
         "excerpt":"리액트에 입문하다  ","categories": ["react"],
         "tags": ["코딩애플_리액트","react"],
         "url": "/react/%EB%A6%AC%EC%95%A1%ED%8A%B8-%EC%9E%85%EB%AC%B8/",
         "teaser": "/assets/images/react.png"
       },{
-        "title": "dart 문법 정리",
-        "excerpt":"최근 처음으로 해커톤에 참가하였다. 오아시스 해커톤이라고, 2021.08.09 ~ 2021.08.20 동안 열리는 호남지역 해커톤이다! 나는 팀원들과 상의하여 flutter 를 이용한 택시 매칭 앱을 만들기로 하였고, 지금 flutter 의 사용 언어인 dart 를 훑어보고 있다. dart 문법 class class constructor 에 관하여. 일반적인 class constructor 사용 class Person { int age; String...","categories": ["undefined"],
+        "title": "[Flutter] dart 문법 정리",
+        "excerpt":"최근 처음으로 해커톤에 참가하였다. 오아시스 해커톤이라고, 2021.08.09 ~ 2021.08.20 동안 열리는 호남지역 해커톤이다! 나는 팀원들과 상의하여 flutter 를 이용한 택시 매칭 앱을 만들기로 하였고, 지금 flutter 의 사용 언어인 dart 를 훑어보고 있다. dart 문법 class class constructor 에 관하여. 일반적인 class constructor 사용 class Person { int age; String...","categories": ["flutter"],
         "tags": ["flutter","dart"],
-        "url": "/undefined/dart-%EB%AC%B8%EB%B2%95/",
+        "url": "/flutter/dart-%EB%AC%B8%EB%B2%95/",
         "teaser": "/assets/images/default.jpeg"
       },{
-        "title": "Future Async Await",
-        "excerpt":"Future future 의 값으로 세가지의 상태가 가능하다. Uncompleted Completed, data return Completed, error return 처음 Future 를 사용해서 data 를 만들면 Uncompleted future 가 들어간다. 이후 Future 가 작업을 끝내면 Completed future 가 들어간다. async, await await 는 async 함수 내에서만 사용 가능하다. async 함수 내에서 await 가 붙은 작업은...","categories": [],
-        "tags": [],
-        "url": "/future-async-await/",
+        "title": "[Flutter] future, async, await 개념 정리",
+        "excerpt":"Future future 의 값으로 세가지의 상태가 가능하다. Uncompleted Completed, data return Completed, error return 처음 Future 를 사용해서 data 를 만들면 Uncompleted future 가 들어간다. 이후 Future 가 작업을 끝내면 Completed future 가 들어간다. async, await await 는 async 함수 내에서만 사용 가능하다. async 함수 내에서 await 가 붙은 작업은...","categories": ["flutter"],
+        "tags": ["flutter","dart"],
+        "url": "/flutter/future-async-await/",
         "teaser": "/assets/images/default.jpeg"
       },{
-        "title": "Stateful Stateless",
-        "excerpt":"stateful 위젯      사용자와 상호작용 존재   재렌더링 가능, 동적 화면 구현   LifeCycle 이 길고 build가 1회 이상 발생   stateless 위젯      사용자와 상호작용 없음   재렌더링 없음, 정적 화면 구현   LifeCycle이 짧고 build가 1회만 발생. 성능이 좋음      React 의 state 를 생각하면 될 듯!   ","categories": [],
-        "tags": [],
-        "url": "/stateful-stateless/",
+        "title": "[Flutter] stateful 위젯이란!?",
+        "excerpt":"stateful 위젯      사용자와 상호작용 존재   재렌더링 가능, 동적 화면 구현   LifeCycle 이 길고 build가 1회 이상 발생   stateless 위젯      사용자와 상호작용 없음   재렌더링 없음, 정적 화면 구현   LifeCycle이 짧고 build가 1회만 발생. 성능이 좋음      React 의 state 를 생각하면 될 듯!   ","categories": ["flutter"],
+        "tags": ["flutter","dart"],
+        "url": "/flutter/stateful-stateless/",
+        "teaser": "/assets/images/default.jpeg"
+      },{
+        "title": "jekyll 코드 폰트 바꾸는 법",
+        "excerpt":"아니, 에 관한 가이드 글은 너무나 많은데, 왜 코드 글씨체 바꾸는 법은 아무도 안알려줄까..!! 본문 글씨체는 내가 바라는 대로 정갈하게 맞추어 놓았지만 코드 글씨체가 너무 귀여워서 안어울린다.. 그래서 이번에는 코드 글씨체를 바꾸는 방법을 알아보고 적용해 보려한다. @font-face 와 font-family ms word 혹은 ms ppt 에서 새로운 서체를 이용하던 순간을 생각해보자....","categories": ["Jekyll"],
+        "tags": ["Jekyll"],
+        "url": "/jekyll/jekyll-code-font/",
         "teaser": "/assets/images/default.jpeg"
       }]
